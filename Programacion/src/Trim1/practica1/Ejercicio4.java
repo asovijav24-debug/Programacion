a09483e7333413b00921863b91cb7fe11b05f0ca
Javier Aso <aso.vijav24@zaragoza.salesianos.edu>
2026-09-24 11:12:43 +0200

    Continuamos con creacion de ejercicio 4

--- Programacion/src/Trim1/practica1/Ejercicio4.java
+++ Programacion/src/Trim1/practica1/Ejercicio4.java
@@ -3,5 +3,6 @@ package Trim1.practica1;
 public class Ejercicio4 {
     public static void main(String[] args){
 
+        System.out.println("Cantidad a retirar positiva y multiplo de 10");
     }
 }
